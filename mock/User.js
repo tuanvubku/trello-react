f5fed48968ae2e63f6854d98c73af05d9d504ea4
Nguyễn Anh Khoa <ng.akhoa@yahoo.com.vn>
2019-04-29 01:45:41 +0700

    add: All common endpoint mockup complete

--- mock/User.js
+++ mock/User.js
@@ -1,11 +1,43 @@
-const currentUser = {
-  user: {
-    name: 'Nguyen Van Teo',
-    age: 21
+const express = require('express');
+const router = express.Router();
+const moment = require('moment');
+
+const sampleUsers = [
+  {
+    _id: 1,
+    username: 'teo',
+    imageUrl: '',
+    password: '123456',
+    dateCreated: moment().subtract(3, 'months'),
+    role: ['user']
   },
-  role: ['admin']
-};
+  {
+    _id: 2,
+    username: 'ti',
+    imageUrl: '',
+    password: '123456',
+    dateCreated: moment().subtract(1, 'days'),
+    role: ['user']
+  },
+  {
+    _id: 3,
+    username: 'ton',
+    imageUrl: '',
+    password: '123456',
+    dateCreated: moment().subtract(1, 'weeks'),
+    role: ['user']
+  },
+  {
+    _id: 4,
+    username: 'tasdfsdaf',
+    imageUrl: '',
+    password: '123456',
+    dateCreated: moment().subtract(4, 'months'),
+    role: ['role']
+  }
+];
 
 module.exports = {
-  currentUser
+  userEndpoint: router,
+  sampleUsers
 };
